5e0a076ff11d8bf74d07278fb046abd74443955f
vamsijagatha3127 <vamsijagatha123@gmail.com>
2024-05-08 01:01:02 +0530

    Heart Animation

--- 0.1Heart-Animation/app.js
+++ 0.1Heart-Animation/app.js
@@ -5,6 +5,7 @@ greyHeart.addEventListener("click", () => {
   redHeart.classList.add("animation");
   greyHeart.classList.add("fill-color");
 });
+
 redHeart.addEventListener("click", () => {
   redHeart.classList.remove("animation");
   greyHeart.classList.remove("fill-color");
